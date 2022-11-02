c4ccfa14d7caaaf3b5388071725546e3011c654e
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2022-11-02 16:08:36 +0100

    bonus

--- script.js
+++ script.js
@@ -2,25 +2,47 @@ const eleGrid = document.querySelector(".grid");
 const eleButton = document.querySelector("button");
 
 eleButton.addEventListener("click", function(){
-    
-        for (let i = 1; i <= 100; i++) {
-            
-            console.log("non premere troppe volte che si rompe")
-            const eleCell = document.createElement("div");
-            eleCell.classList.add("cell");
-            eleCell.innerHTML = i
-            eleGrid.append(eleCell)
-            
-            
-             
-             
-    
-            eleCell.addEventListener('click', function () {
-                this.classList.toggle('active')
-                console.log("hai selezionato la cella: " + i)
-            });
-        }
-    
+    const difficultySelect = document.querySelector("select").value;
 
-})
+    if (eleGrid.innerHTML === "") {
+        if (difficultySelect === "easy") {
+            for (let i = 1; i <= 100; i++) {
+                eleCell = document.createElement("div");
+                eleCell.classList.add("cell");
+                eleCell.innerHTML = i
+                eleGrid.append(eleCell)
+                eleCell.addEventListener('click', function () {
+                    this.classList.toggle('active')
+                    console.log("hai selezionato la cella: " + i)
+                });
+        } 
+        } else if (difficultySelect === "medium") {
+            for (let i = 1; i <= 81; i++) {
+                eleCell = document.createElement("div");
+                eleCell.classList.add("cell");
+                eleCell.classList.add("medium")
+                eleCell.innerHTML = i
+                eleGrid.append(eleCell)
+                eleCell.addEventListener('click', function () {
+                    this.classList.toggle('active')
+                    console.log("hai selezionato la cella: " + i)
+                });
+            }
+        } else if (difficultySelect === "hard") {
+            for (let i = 1; i <= 49; i++) {
+                eleCell = document.createElement("div");
+                eleCell.classList.add("cell");
+                eleCell.classList.add("big")
+                eleCell.innerHTML = i
+                eleGrid.append(eleCell) 
+                eleCell.addEventListener('click', function () {
+                    this.classList.toggle('active')
+                    console.log("hai selezionato la cella: " + i)
+                });
+                
+            }
+        }
+    }
+
     
+})
